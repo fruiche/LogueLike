789b67dac7d038d5672bc7a00d319c1ac4b59a0c
fruiche <fruitcakesyndrome256@gmail.com>
2019-04-06 06:05:59 +0900

    first_project
    最初のプロジェクト

---
+++ LogueLike/core/src/dungeon/playScreen.java
@@ -0,0 +1,62 @@
+package dungeon;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class playScreen implements Screen {
+
+	private gameMain game;
+	SpriteBatch batch;
+	Texture img;
+
+	public playScreen(gameMain game) {
+		this.game = game;
+		batch = new SpriteBatch();
+		img = new Texture("core/assets/badlogic.jpg");
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void render(float delta) {
+
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
+}
---
+++ LogueLike/desktop/src/dungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package dungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import dungeon.gameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new gameMain(), config);
+	}
+}
